ca5b45abd9d1cce418c88d6bf5ea0057e95802c5
DESKTOP-1MHKANA\Christian <chris.hoffmann84@gmail.com>
2017-11-16 11:42:09 +0100

    - added crest - replaced hello world with example request page

--- js/pcars.js
+++ js/pcars.js
@@ -1,7 +1,36 @@
 (function() {
+  let server = 'localhost';
+  let port = '8080';
+  let uri = '/crest/v1/api';
+  
+  function log(msg) {  
+    console.log(msg);
+    $('#status').text(msg);
+  }
+  
+  function sendRequest() {
+    // fire a request against the api, docs at
+    // https://github.com/NLxAROSA/CREST/tree/master
+
+    params = undefined;
+    //params = 'buildInfo=true';
+
+    $.ajax({
+      url: 'http://' + server + ':' + port + uri + (params ? '?' + params : ''),
+      async: true,
+      error: (jqXHR, status, error) => log('An Error occured! Status: ' + status + ', Message: ' + error),
+      success: (data, textStatus, jqXHR) => {
+        log('Response received, Status: ' + textStatus);
+        $('#response').text(JSON.stringify(data, null, 4));
+        console.log(data);
+      }
+    });
+  }
+
   $(document).ready(() => {
-    $('button').click(function() { 
-      $('p').text("First Heartbeat!");
+    $('#fireAjax').click(function() {
+      log('Sending Request...');
+      sendRequest();
     });
   });
 })();

7e90d43694af3dc8995965cd08b63f77aaed6534
DESKTOP-1MHKANA\Christian <chris.hoffmann84@gmail.com>
2017-11-16 12:07:24 +0100

    added polling

--- js/pcars.js
+++ js/pcars.js
@@ -2,25 +2,49 @@
   let server = 'localhost';
   let port = '8080';
   let uri = '/crest/v1/api';
+
+  let pollingDelay = 1000;
+  var intervalId = undefined;
+  var requestId = 0;
   
   function log(msg) {  
     console.log(msg);
     $('#status').text(msg);
   }
-  
+
+  function startPolling() {
+    if (!intervalId) {      
+      sendRequest();
+      intervalId = setInterval(() => {
+        sendRequest();
+      }, pollingDelay);
+    }
+  }
+
+  function stopPolling() {
+    clearInterval(intervalId);
+    intervalId = undefined;
+  }
+
   function sendRequest() {
+    requestId++;
+    sendAjaxRequest(requestId);
+  }
+  
+  function sendAjaxRequest(id) {
     // fire a request against the api, docs at
     // https://github.com/NLxAROSA/CREST/tree/master
 
     params = undefined;
     //params = 'buildInfo=true';
 
+    log('Sending Request ' + id + '...');
     $.ajax({
       url: 'http://' + server + ':' + port + uri + (params ? '?' + params : ''),
       async: true,
-      error: (jqXHR, status, error) => log('An Error occured! Status: ' + status + ', Message: ' + error),
+      error: (jqXHR, status, error) => log('Request ' + id + ': An Error occured! Status: ' + status + ', Message: ' + error),
       success: (data, textStatus, jqXHR) => {
-        log('Response received, Status: ' + textStatus);
+        log('Request ' + id + ': Response received, Status: ' + textStatus);
         $('#response').text(JSON.stringify(data, null, 4));
         console.log(data);
       }
@@ -28,9 +52,20 @@
   }
 
   $(document).ready(() => {
-    $('#fireAjax').click(function() {
-      log('Sending Request...');
+    $('#fireAjax').click(() => {
       sendRequest();
     });
+
+    $('#startPolling').click(() => {
+      $('#startPolling').prop('disabled', true);
+      $('#stopPolling').prop('disabled', false);
+      startPolling();
+    });
+
+    $('#stopPolling').click(() => {
+      $('#startPolling').prop('disabled', false);
+      $('#stopPolling').prop('disabled', true);
+      stopPolling();
+    })
   });
 })();
